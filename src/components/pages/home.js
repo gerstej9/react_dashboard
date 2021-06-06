df3ba2cd06613405deea19f16d9c8f3013c74035
James Gerstenberger <gerstej9@gmail.com>
2021-06-06 16:50:52 -0700

    Added reducer and query for leaderboard retrieval

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,15 +1,16 @@
 import { useEffect } from 'react';
 import { connect } from "react-redux";
 import { getNmrPrice } from '../../store/nmr.reducer.js';
-
+import { getLeaderboard } from '../../store/leaderboard.reducer.js';
 
 function HomePage(props){
   
   useEffect (() => {
     props.getNmrPrice();
+    props.getLeaderboard();
   }, [])
 
-  console.log(props.nmr);
+  console.log(props.leaderboard);
 
   return (
     <h1>${props.nmr.nmrPrice}</h1>
@@ -19,11 +20,13 @@ function HomePage(props){
 const mapStateToProps = state => {
   return {
     nmr: state.nmr,
+    leaderboard: state.leaderboard,
   }
 }
 
 const mapDispatchToProps = {
   getNmrPrice,
+  getLeaderboard,
 }
 
 
--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import nmrReducer from './nmr.reducer.js';
+import leaderboardReducer from './leaderboard.reducer.js';
 
 
 let reducers = combineReducers({
   nmr: nmrReducer,
+  leaderboard: leaderboardReducer,
 })
 
 let store = () => {
